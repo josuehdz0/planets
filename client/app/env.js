47d3819669ded451bd83fa677727894d8800cb30
Josue Hernandez <josuehdz0@gmail.com>
2023-02-15 15:34:11 -0700

    first commit

---
+++ client/app/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'josuehdz0.us.auth0.com'
+export const audience = 'https://nube.com'
+export const clientId = '5FoHweUt7qPYObh7UIG9KlCiw38fTTAZ'
